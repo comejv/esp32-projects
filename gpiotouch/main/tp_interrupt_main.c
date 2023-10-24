1896b1d26fbcd58dbb98d50fd6bd1825fab96c93
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2023-10-24 18:46:16 +0200

    touch pad interrupt delete filtering mode

--- gpiotouch/main/tp_interrupt_main.c
+++ gpiotouch/main/tp_interrupt_main.c
@@ -17,8 +17,8 @@
 
 static const char *TAG = "Touch pad";
 
-#define TOUCH_THRESH_NO_USE   (0)
-#define TOUCH_THRESH_PERCENT  (80)
+#define TOUCH_THRESH_NO_USE (0)
+#define TOUCH_THRESH_PERCENT (80)
 #define TOUCHPAD_FILTER_TOUCH_PERIOD (10)
 #define TOUCH_PAD_N (4)
 
@@ -37,14 +37,14 @@ static uint32_t s_pad_init_val[TOUCH_PAD_N];
 static void tp_example_set_thresholds(void)
 {
     uint16_t touch_value;
-    for (int i = 0; i < TOUCH_PAD_N; i++) {
-        //read filtered value
+    for (int i = 0; i < TOUCH_PAD_N; i++)
+    {
+        // read filtered value
         touch_pad_read_filtered(i, &touch_value);
         s_pad_init_val[i] = touch_value;
         ESP_LOGI(TAG, "test init: touch pad [%d] val is %d", i, touch_value);
-        //set interrupt threshold.
+        // set interrupt threshold.
         ESP_ERROR_CHECK(touch_pad_set_thresh(i, touch_value * 2 / 3));
-
     }
 }
 
@@ -68,55 +68,33 @@ static void tp_example_set_thresholds(void)
 static void tp_example_read_task(void *pvParameter)
 {
     static int show_message;
-    int change_mode = 0;
-    int filter_mode = 0;
-    while (1) {
-        if (filter_mode == 0) {
-            //interrupt mode, enable touch interrupt
-            touch_pad_intr_enable();
-            for (int i = 0; i < TOUCH_PAD_N; i++) {
-                if (s_pad_activated[i] == true) {
-                    ESP_LOGI(TAG, "T%d activated!", i);
-                    // Wait a while for the pad being released
-                    vTaskDelay(200 / portTICK_PERIOD_MS);
-                    // Clear information on pad activation
-                    s_pad_activated[i] = false;
-                    // Reset the counter triggering a message
-                    // that application is running
-                    show_message = 1;
-                }
-            }
-        } else {
-            //filter mode, disable touch interrupt
-            touch_pad_intr_disable();
-            touch_pad_clear_status();
-            for (int i = 0; i < TOUCH_PAD_N; i++) {
-                uint16_t value = 0;
-                touch_pad_read_filtered(i, &value);
-                if (value < s_pad_init_val[i] * TOUCH_THRESH_PERCENT / 100) {
-                    ESP_LOGI(TAG, "T%d activated!", i);
-                    ESP_LOGI(TAG, "value: %"PRIu16"; init val: %"PRIu32, value, s_pad_init_val[i]);
-                    vTaskDelay(200 / portTICK_PERIOD_MS);
-                    // Reset the counter to stop changing mode.
-                    change_mode = 1;
-                    show_message = 1;
-                }
+    while (1)
+    {
+        // interrupt mode, enable touch interrupt
+        touch_pad_intr_enable();
+        for (int i = 0; i < TOUCH_PAD_N; i++)
+        {
+            if (s_pad_activated[i] == true)
+            {
+                ESP_LOGI(TAG, "T%d activated!", i);
+                // Wait a while for the pad being released
+                vTaskDelay(200 / portTICK_PERIOD_MS);
+                // Clear information on pad activation
+                s_pad_activated[i] = false;
+                // Reset the counter triggering a message
+                // that application is running
+                show_message = 1;
             }
         }
+    }
 
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
 
-        // If no pad is touched, every couple of seconds, show a message
-        // that application is running
-        if (show_message++ % 500 == 0) {
-            ESP_LOGI(TAG, "Waiting for any pad being touched...");
-        }
-        // Change mode if no pad is touched for a long time.
-        // We can compare the two different mode.
-        if (change_mode++ % 2000 == 0) {
-            filter_mode = !filter_mode;
-            ESP_LOGW(TAG, "Change mode...%s", filter_mode == 0 ? "interrupt mode" : "filter mode");
-        }
+    // If no pad is touched, every couple of seconds, show a message
+    // that application is running
+    if (show_message++ % 500 == 0)
+    {
+        ESP_LOGI(TAG, "Waiting for any pad being touched...");
     }
 }
 
@@ -127,10 +105,12 @@ static void tp_example_read_task(void *pvParameter)
 static void tp_example_rtc_intr(void *arg)
 {
     uint32_t pad_intr = touch_pad_get_status();
-    //clear interrupt
+    // clear interrupt
     touch_pad_clear_status();
-    for (int i = 0; i < TOUCH_PAD_N; i++) {
-        if ((pad_intr >> i) & 0x01) {
+    for (int i = 0; i < TOUCH_PAD_N; i++)
+    {
+        if ((pad_intr >> i) & 0x01)
+        {
             s_pad_activated[i] = true;
         }
     }
@@ -141,8 +121,9 @@ static void tp_example_rtc_intr(void *arg)
  */
 static void tp_example_touch_pad_init(void)
 {
-    for (int i = 0; i < TOUCH_PAD_N; i++) {
-        //init RTC IO and mode for touch pad.
+    for (int i = 0; i < TOUCH_PAD_N; i++)
+    {
+        // init RTC IO and mode for touch pad.
         touch_pad_config(i, TOUCH_THRESH_NO_USE);
     }
 }
@@ -167,5 +148,5 @@ void app_main(void)
     // Register touch interrupt ISR
     touch_pad_isr_register(tp_example_rtc_intr, NULL);
     // Start a task to show what pads have been touched
-    xTaskCreate(&tp_example_read_task, "touch_pad_read_task", 4096, NULL, 5, NULL);
+    xTaskCreate(&tp_example_read_task, "touch_pad_read_task", 4096, NULL, tskIDLE_PRIORITY, NULL);
 }
